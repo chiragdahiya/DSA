70e7d283341af06e0520fd4cff34364fd8bef414
chiragdahiya <82759466+chiragdahiya@users.noreply.github.com>
2025-09-23 15:54:23 +0530

    questions

---
+++ Lec4/7.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int i;
+    int count=0;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        if(n%i==0){ count++;
+
+
+
+
+        }
+       
+
+
+
+
+    }
+     if(count==2){
+            cout<<"it is a prime no";
+        }
+       else{
+        cout<<"not a prime number";
+       }
+return 0;  
+}
---
+++ Lec4/8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1, n2;
+    cin>>n1>>n2;
+    int GCD=1;
+    for(int i=1;i<=min(n1,n2);i++){
+        if(n1%i==0 && n2%i==0){
+            GCD=i;
+           
+        }
+        
+    }
+    cout<<GCD<<endl;
+}
