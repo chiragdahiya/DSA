3d0e25927b6a4b3585b0d7417ae9fdf61d310ad8
chiragdahiya <82759466+chiragdahiya@users.noreply.github.com>
2025-10-18 23:07:13 +0530

    qyes

---
+++ Arrays/findNumberonce.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+    for(int i=0;i<n;i++){
+       int num = arr[i];
+        int count = 0;
+
+    for(int j=0;j<n;j++){
+        if(arr[j] == num){
+            count++;
+        }
+    }
+
+    if(count == 1){
+        cout<<num<<" ";
+    }
+    }
+
+}
