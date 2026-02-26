8ed39b573f02e5c4d795cb9efb52ef7a5debd1dd
Chirag-Dahiya <82759466+chiragdahiya@users.noreply.github.com>
2026-02-26 23:31:40 +0530

    Reverse

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,7 +1,28 @@
 package Strings;
 
 public class ReverseString {
-    public static void main(String args[]){
-        
+
+    public static void main(String[] args) {
+
+        String s = "hello world this is java";
+
+        int n = s.length();
+        String ans = "";
+        s = new StringBuilder(s).reverse().toString();
+
+        for (int i = 0; i < n; i++) {
+
+            String word = "";
+            while (i < n && s.charAt(i) != ' ') {
+                word += s.charAt(i);
+                i++;
+            }
+            word = new StringBuilder(word).reverse().toString();
+
+            if (word.length() > 0) {
+                ans += word + " ";
+            }
+        }
+        System.out.println(ans.trim());
     }
 }
