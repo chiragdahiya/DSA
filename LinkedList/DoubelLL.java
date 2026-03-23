f09b775802ff96194d81a0d6086a3fe9da8e2d41
chiragdahiya <82759466+chiragdahiya@users.noreply.github.com>
2026-03-24 00:06:56 +0530

    Double LL

--- LinkedList/DoubelLL.java
+++ LinkedList/DoubelLL.java
@@ -49,12 +49,47 @@ public class DoubelLL {
 
         System.out.println("null");
 }
+
+public void pop_front(){
+      if(head == null){
+        System.out.println("its empty");
+        return;
+      }
+
+      Node temp = head;
+      head = head.next;
+
+      if(head!=null){
+        head.prev = null;
+      }else{
+        tail = null;
+      }
+      temp.next = null;
+     
+}
+
+public void pop_back(){
+    if(head == null){
+        System.out.println("empty");
+    }
+
+    Node temp = tail;
+    tail = tail.prev;
+
+    if(tail!=null){
+        tail.next= null;
+    }
+    temp.prev = null;
+}
+
     public static void main(String[] args){
 
         DoubelLL dll = new DoubelLL();
                     dll.push_front(1);
                     dll.push_front(2);
                     dll.push_front(3);
+                    dll.pop_front();
+                    dll.pop_back();
                     dll.printList();
                     
     }
