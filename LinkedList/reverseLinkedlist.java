ba8e87cb08933657b4616da201185571ca09189b
Chirag-Dahiya <dahiyachirag2018@gmail.com>
2026-03-26 23:15:27 +0530

    reverseLl

---
+++ LinkedList/reverseLinkedlist.java
@@ -0,0 +1,70 @@
+package LinkedList;
+import java.util.Stack;
+public class reverseLinkedlist {
+  Node head;
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void reverse(){
+         Stack<Node> stack = new Stack<>();
+           Node temp = head;
+           while(temp!=null){
+            stack.push(temp);
+            temp=temp.next;
+           }
+           head = stack.pop();
+              temp = head;
+             
+
+              while(!stack.isEmpty()){
+              Node curr = stack.pop();
+                temp.next = curr;
+                temp = curr;
+
+
+
+
+
+        
+    }
+    temp.next = null;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+public static void main(String[] args) {
+    reverseLinkedlist list = new reverseLinkedlist();
+    list.addFirst(1);
+    list.addFirst(2);
+    list.addFirst(3);
+    list.addFirst(4);
+    System.out.println("Original List:");
+    list.printList();
+    list.reverse();
+    System.out.println("Reversed List:");
+    list.printList();
+}
+}
