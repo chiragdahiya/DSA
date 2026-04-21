4d8df6cf5c87c88e49b99b344178f2f68b115941
Chirag-Dahiya <dahiyachirag2018@gmail.com>
2026-04-21 23:22:40 +0530

    recusrdion

---
+++ Recurssion/First.java
@@ -0,0 +1,22 @@
+import java.util.*;
+package Recurssion;
+
+public class First {
+
+    public static void printNo(int n){
+
+        if(n==0){
+            return;
+        }
+        system.out.println(n);
+        printNo(n-1);
+    }
+
+
+
+    public static void main(String[] args) {
+       int n=5;
+       printNo(n);
+    }
+    
+}
