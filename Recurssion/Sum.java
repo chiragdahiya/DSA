f6ab906ee89323c7d92f6f339d65e0dbe42b06e7
Chirag-Dahiya <dahiyachirag2018@gmail.com>
2026-04-21 23:33:49 +0530

    Sum

---
+++ Recurssion/Sum.java
@@ -0,0 +1,24 @@
+package Recurssion;
+
+public class Sum {
+
+    public static void SumN(int i, int n, int sum){
+
+if(i==n){
+    sum += i;
+    System.out.println(sum);
+    return;
+}
+        sum +=i;
+        SumN(i+1,n,sum);
+
+    }
+
+
+
+    public static void main(String[] args){
+
+        SumN(1,10,0);
+    }
+    
+}
