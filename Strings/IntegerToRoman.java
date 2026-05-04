74b52d3e918dd759524a5a6a2ab6ffde8453f281
Chirag-Dahiya <dahiyachirag2018@gmail.com>
2026-05-04 22:01:28 +0530

    String manipulation

---
+++ Strings/IntegerToRoman.java
@@ -0,0 +1,39 @@
+package Strings;
+
+public class IntegerToRoman {
+
+    public static String intToRoman(int num) {
+
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            int count = num / values[i];
+
+            if (count != 0) {
+                result.append(symbols[i].repeat(count));
+                num %= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        int num = 1994;
+
+        String roman = intToRoman(num);
+        System.out.println("Roman: " + roman);
+    }
+}
