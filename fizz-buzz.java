586e5bf4b847691f9cb49bc439d054e200eb36d8
Chirag Dahiya <82759466+chiragdahiya@users.noreply.github.com>
2026-07-19 21:03:25 +0530

    Add LeetCode solution: 412. Fizz Buzz

--- fizz-buzz.java
+++ fizz-buzz.java
@@ -1,3 +1,18 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```java []
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> answer = new ArrayList<>();
@@ -20,3 +35,4 @@ class Solution {
         return answer;
     }
 }
+```
