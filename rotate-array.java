7784e93857269d1ad335b3b04a22e002e62f0eff
Chirag Dahiya <82759466+chiragdahiya@users.noreply.github.com>
2026-08-06 00:34:43 +0530

    Add LeetCode solution: 189. Rotate Array

--- rotate-array.java
+++ rotate-array.java
@@ -1,16 +1,22 @@
 class Solution {
+
+    public void reverse(int [] nums, int left, int right){
+       while(left<right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+    }
+   
+    }
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k%=n;
 
-        for(int step=0;step<k;step++){
-            int last = nums[n-1];
-            for(int i = n-1;i>0;i--){
-                nums[i] = nums[i-1];
-            }
-               nums[0]=last;
-        }
-     
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
         
     }
 }
